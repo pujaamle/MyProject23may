5583535ec80c9f13adf180c2ae51ee9dc2fbe41b
karmurep <105978687+karmurep@users.noreply.github.com>
2022-05-23 23:48:49 +0530

    up-commit
    check it and merge

---
+++ mygitpro/src/Myupdate.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Myupdate {
+
+	public static void main(String[] args) {
+		System.out.println("we say worlds' to Hello Everyone");
+		System.out.println("lai easy ahe bhida phakt");
+
+	}
+
+}
